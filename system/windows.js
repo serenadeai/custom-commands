27c87ec5d80506cc06d6fa281be46225c08bf089
Mark Phillipson <MPhillipson0@Gmail.com>
2021-03-01 19:07:02 +0000

    Removed comment line

--- system/windows.js
+++ system/windows.js
@@ -3,10 +3,6 @@ serenade.global().command("curly brackets in",async api =>{
     await api.pressKey("left")
 });
 serenade.global().command("curly brackets out",async api =>{
-    //await api.focus("terminal");
     await api.typeText("{}");
     await api.pressKey("space")
-});
-serenade.global().command("focus terminal",async api =>{
-    await api.pressKey("ctrl+alt+shift+t");
 });
