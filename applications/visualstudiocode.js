b950f0b9c94187591270be813cc8565803161578
Mark Phillipson <MPhillipson0@Gmail.com>
2022-01-02 18:37:43 +0000

    using evaluate in plugin where available

--- applications/visualstudiocode.js
+++ applications/visualstudiocode.js
@@ -1,21 +1,19 @@
-
 serenade.app("code").command("close all tabs", async (api) => {
-	await api.pressKey("k", ["commandOrControl"]);
-	await api.pressKey("w");
+	await api.evaluateInPlugin("openEditors.closeAll")
 });
 serenade.app("code").command("show explorer", async (api, matches) => {
-	await api.pressKey("l", ["commandOrControl", "alt"]);
+	await api.evaluateInPlugin("workbench.view.explorer");
 });
 
 serenade.app("code").command("toggle sidebar", async (api, matches) => {
-	await api.pressKey("b", ["commandOrControl", "alt"]);
+	await api.evaluateInPlugin("workbench.action.toggleSidebarVisibility")
 });
 serenade.app("code").command("search files", async (api, matches) => {
 	await api.pressKey("f1");
 	await api.pressKey("backspace");
 });
 serenade.app("code").command("toggle terminal", async (api, matches) => {
-	await api.pressKey("'", ["commandOrControl", "alt"]);
+	await api.evaluateInPlugin("workbench.action.terminal.toggleTerminal")
 });
 serenade.app("code").command("following issue", async (api, matches) => {
 	await api.pressKey("f8", ["alt"]);
@@ -23,68 +21,71 @@ serenade.app("code").command("following issue", async (api, matches) => {
 serenade.app("code").command("previous issue", async (api, matches) => {
 	await api.pressKey("f8", ["alt", "shift"]);
 });
-// this does ! appear to work
-serenade.app("code").command("toggle cursor", async (api, matches) => {
-	await api.pressKey("m", ["shift", "alt"]);
-});
 serenade.app("code").command("select matching", async (api, matches) => {
-	await api.pressKey(".", ["alt", "shift"]);
+	await api.evaluateInPlugin("editor.action.addSelectionToNextFindMatch")
 });
+//select all occurrences of find match
 serenade.app("code").command("select all matching", async (api, matches) => {
-	await api.pressKey("l", ["commandOrControl", "shift", "alt"]);
+	await api.pressKey("l", ["control", "shift", "alt"]);
 });
 serenade.app("code").command("select matching <%number%>", async (api, matches) => {
 	const increment = parseInt(matches.number);
 	for (let index = 0; index < increment; index++) {
-		await api.pressKey(".", ["alt", "shift"]);
+		await api.evaluateInPlugin("openEditors.closeAll");
 	}
 });
 serenade.app("code").command("select a language <%language%>", async (api, matches) => {
-	await api.pressKey("k", ["commandOrControl"]);
+	await api.pressKey("k", ["control"]);
 	await api.pressKey("m");
 	await api.typeText(matches.language);
 });
 serenade.app("code").command("open folder", async (api) => {
-	await api.pressKey("k", ["commandOrControl"]);
-	await api.pressKey("o", ["commandOrControl"]);
+	await api.evaluateInPlugin("workbench.action.files.openLocalFolder")
 });
-serenade.app("code").command("expand selection", async (api,matches) => {
-	await api.pressKey("w", ["commandOrControl"]);
+serenade.app("code").command("expand selection", async (api, matches) => {
+	await api.evaluateInPlugin("editor.action.smartSelect.expand")
 });
-serenade.app("code").command("keyboard shortcuts", async (api,matches) => {
-	await api.pressKey("k", ["commandOrControl"]);
-	await api.pressKey("s", ["commandOrControl"]);
+serenade.app("code").command("keyboard shortcuts", async (api, matches) => {
+	await api.pressKey("k", ["control"]);
+	await api.pressKey("s", ["control"]);
 });
-//Markers are for CodeRush extension:
-serenade.app("code").command("drop marker", async (api,matches) => {
-	await api.pressKey("home", ["alt"]);
+//this is for CodeRush extension:
+serenade.app("code").command("drop marker", async (api, matches) => {
+	await api.evaluateInPlugin("CodeRush.markerDrop")
 });
-// this was originally escape, but changed as it interfered with other commands
-serenade.app("code").command("collect marker", async (api,matches) => {
-	await api.pressKey("e", ["commandOrControl", "shift", "alt"]);
+serenade.app("code").command("collect marker", async (api, matches) => {
+	await api.evaluateInPlugin("CodeRush.markerCollect")
 });
-serenade.app("code").command("swap marker", async (api,matches) => {
-	await api.pressKey("home", ["shift", "alt"]);
+serenade.app("code").command("swap marker", async (api, matches) => {
+	await api.evaluateInPlugin("CodeRush.markerSwap")
 });
-serenade.app("code").command("open smart tag", async (api,matches) => {
-	await api.pressKey(".", ["commandOrControl"]);
+//Show Code Actions
+serenade.app("code").command("open smart tag", async (api, matches) => {
+	await api.pressKey(".", ["control"]);
 });
-serenade.app("code").command("show extensions", async (api,matches) => {
-	await api.pressKey("x", ["commandOrControl", "shift"]);
+serenade.app("code").command("show extensions", async (api, matches) => {
+	await api.pressKey("x", ["control", "shift"]);
 });
-serenade.app("code").command("advanced new file", async (api,matches) => {
-	await api.pressKey("n", ["commandOrControl", "alt"]);
+serenade.app("code").command("advanced new file", async (api, matches) => {
+	await api.evaluateInPlugin("extension.advancedNewFile")
 });
-serenade.app("code").command("open new terminal", async (api,matches) => {
-	await api.pressKey("c", ["commandOrControl", "shift"]);
+//unfortunately cannot get this to work
+serenade.app("code").command("add cursors to line ends", async (api, matches) => {
+	await api.evaluateInPlugin("editor.action.insertCursorAtEndOfEachLineSelected)")
 });
-serenade.app("code").command("clear terminal", async (api,matches) => {
-	await api.pressKey("c", ["shift", "alt"]);
-	await api.pressKey("c", ["shift", "alt"]);
+serenade.app("code").command("open new terminal", async (api, matches) => {
+	await api.evaluateInPlugin("workbench.action.terminal.openNativeConsole")
 });
-serenade.app("code").command("resize terminal down", async (api,matches) => {
-	await api.pressKey("down", ["win", "alt"]);
+serenade.app("code").command("clear terminal", async (api, matches) => {
+	await api.evaluateInPlugin("workbench.action.terminal.clear")
 });
-serenade.app("code").command("resize terminal up", async (api,matches) => {
-	await api.pressKey("up", ["win", "alt"]);
+
+serenade.app("code").command("resize terminal down", async (api, matches) => {
+	await api.evaluateInPlugin("workbench.action.terminal.resizePaneDown")
+});
+serenade.app("code").command("resize terminal up", async (api, matches) => {
+	await api.evaluateInPlugin("workbench.action.terminal.resizePaneUp")
+});
+serenade.app("code").command("show source control", async (api, matches) => {
+	await api.pressKey("g", ["control", "shift"]);
 });
